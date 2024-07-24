b3e6b379f0150d9322beba9fa27fc8063ddcafc6
AlejandroToledo2024 <alyo20111@gmail.com>
2024-07-24 17:00:55 -0300

    primer commit

--- primeraEntrega.js
+++ primeraEntrega.js
@@ -11,4 +11,3 @@ let eleccionSillones = prompt(eleccionUsuario2);
 let eleccionSommier = prompt(eleccionUsuario3);
 let eleccionEscritorio = prompt(eleccionUsuario4);
 
-console.log("usted gasto ", eleccion1 + eleccion2 + eleccion3 + eleccion4 );
