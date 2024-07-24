2fae62dc5577b38a6f160cb6cb633298d433807e
AlejandroToledo2024 <alyo20111@gmail.com>
2024-07-24 16:57:42 -0300

    primer commit de preEntrega1

---
+++ primeraEntrega.js
@@ -0,0 +1,14 @@
+alert("Bienvenido a Muebleria FEBE")
+alert("A copntinuacion elija un opcion :");
+let eleccion = prompt("1)-juego de comedor 2)-Sillones 3)-Sommier 4)-Escritorios 5)-Finalizar")
+const juegoComedor = 12000 ;
+const sillones = 10000;
+const sommier = 15000;
+const escritorior = 9000;
+
+let eleccionComedor = prompt(eleccionUsuario1);
+let eleccionSillones = prompt(eleccionUsuario2);
+let eleccionSommier = prompt(eleccionUsuario3);
+let eleccionEscritorio = prompt(eleccionUsuario4);
+
+console.log("usted gasto ", eleccion1 + eleccion2 + eleccion3 + eleccion4 );
